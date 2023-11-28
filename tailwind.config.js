c713f7571afa85cec20551cccca15208219a3ae0
gem-manvijain <manvi.jain@geminisolutions.com>
2023-11-28 10:23:54 +0530

    add tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,13 @@
 export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
 export const theme = {
 	extend: {},
+	colors: {
+		'vivid-orange': '#FF6002',
+		'white-smoke': '#F5F5F5',
+		bashful: '#E4EDED',
+	},
+	fontFamily: {
+		Montserrat: ['Montserrat'],
+	},
 };
 export const plugins = [];
